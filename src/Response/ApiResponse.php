feb3a693b29674e9c6af8d223a29aba1cd6ae79b
Stepan Anchugov <kixxx1@gmail.com>
2016-04-02 16:28:56 +0500

    Make ApiResponse implement ResponseInterface

--- src/Response/ApiResponse.php
+++ src/Response/ApiResponse.php
@@ -3,11 +3,12 @@
 namespace Kix\Apiranha\Response;
 
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 
 /**
  * Class ApiResponse
  */
-class ApiResponse
+class ApiResponse implements ResponseInterface
 {
     private $wrapped;
     
@@ -30,5 +31,75 @@ class ApiResponse
     public function getData()
     {
         return $this->data;
+    }
+
+    public function getProtocolVersion()
+    {
+        return $this->wrapped->getProtocolVersion();
+    }
+
+    public function withProtocolVersion($version)
+    {
+        return $this->wrapped->withProtocolVersion($version);
+    }
+
+    public function getHeaders()
+    {
+        return $this->wrapped->getHeaders();
+    }
+
+    public function hasHeader($name)
+    {
+        return $this->wrapped->hasHeader($name);
+    }
+
+    public function getHeader($name)
+    {
+        return $this->wrapped->getHeader($name);
+    }
+
+    public function getHeaderLine($name)
+    {
+        return $this->wrapped->getHeaderLine($name);
+    }
+
+    public function withHeader($name, $value)
+    {
+        return $this->wrapped->withHeader($name, $value);
+    }
+
+    public function withAddedHeader($name, $value)
+    {
+        return $this->wrapped->withAddedHeader($name, $value);
+    }
+
+    public function withoutHeader($name)
+    {
+        return $this->wrapped->withoutHeader($name);
+    }
+
+    public function getBody()
+    {
+        return $this->wrapped->getBody();
+    }
+
+    public function withBody(StreamInterface $body)
+    {
+        return $this->wrapped->withBody($body);
+    }
+
+    public function getStatusCode()
+    {
+        return $this->wrapped->getStatusCode();
+    }
+
+    public function withStatus($code, $reasonPhrase = '')
+    {
+        return $this->wrapped->withStatus($code, $reasonPhrase);
+    }
+
+    public function getReasonPhrase()
+    {
+        return $this->wrapped->getReasonPhrase();
     }
 }
